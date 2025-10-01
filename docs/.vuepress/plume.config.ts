324c4b1f80ade39494efb141aadca8cca0671aab
SuniRein <sunirein@qq.com>
2025-10-01 15:09:35 +0800

    添加备案信息

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -9,6 +9,15 @@ export default defineThemeConfig({
         location: '中国 杭州',
         circle: true,
     },
+    footer: {
+        message: `
+            <span style="display: flex; align-items: center; justify-content: center; gap: 3px;">
+                <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2024324598号</a>
+                <img src="/images/gongan.webp">
+                <a href="https://beian.mps.gov.cn/#/query/webSearch?code=33010602013756" rel="noopener" target="_blank">浙公网安备33010602013756号</a>
+            </span>
+        `,
+    },
     navbar: [
         { text: '首页', link: '/' },
         { text: '博客', link: '/blog/' },
